a7b7fc23773554f3ed1ea2551b9d1f5f75dc9ebd
Henry Zou <zouhenry@gmail.com>
2018-11-23 10:59:03 -0500

    adding WallabyJs, Unstated, Material UI Next

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "unstated";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider><App /></Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
